1d90538026f94225423075e2ef1380e3e7fedaeb
Dani_Pop <danielding81133@yahoo.com>
2022-08-08 23:33:06 +0300

    Add project files.

---
+++ EFCoreRelationships/CreateCharacterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreRelationships
+{
+    public class CreateCharacterDTO
+    {
+        public string Name { get; set; } = "Character";
+        public string Class { get; set; } = "Knight";
+        public int UserId { get; set; } = 1;
+    }
+}
